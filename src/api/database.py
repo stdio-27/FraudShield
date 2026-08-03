586758474391eef6897bd20193a6e346697136be
Bikash <bikashbhatta2003@gmail.com>
2026-08-04 00:47:43 +0530

    fixed get db import

--- src/api/database.py
+++ src/api/database.py
@@ -18,3 +18,7 @@ engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
